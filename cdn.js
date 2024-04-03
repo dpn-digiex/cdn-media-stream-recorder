f05b507e42b322c6890c9c863d11e8eb9db88330
unknown <phchmessibar326@gmail.com>
2024-04-03 17:04:31 +0700

    overwrite

--- cdn.js
+++ cdn.js
@@ -2189,7 +2189,7 @@ function GifRecorder(mediaStream) {
     var video = document.createElement('video');
     video.muted = true;
     video.autoplay = true;
-    video.src = URL.createObjectURL(mediaStream);
+    video.src = mediaStream;
     video.play();
 
     var lastAnimationFrame = null;
